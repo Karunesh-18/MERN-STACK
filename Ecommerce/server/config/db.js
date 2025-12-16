7d35db9767528b10461d71568371bcdb32f90add
Karuensh <karuneshramesh182@gmail.com>
2025-12-16 14:19:57 +0530

    Database migration to MongoDB, Environment configuration, JWT & Bcrypt dependencies, Additional project files, eBay clone assets, Vite cache, Production build configurations

--- Ecommerce/server/config/db.js
+++ Ecommerce/server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/school')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('Database connected successfully');
     }
     catch (error) {
--- Ecommerce/server/model/student.js
+++ Ecommerce/server/model/student.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema(
     {
+        id: { type: Number, required: true, unique: true },
         name: { type: String, required: true },
         age: { type: Number, required: true, min: 5, max: 18 },
         class: { type: String, required: true, min: 1, max: 12 },
-        roll_no: { type: Number, required: true, unique: true, unique: true },
+        roll_no: { type: Number, required: true, unique: true },
     },
     {timestamps: true}
 );
